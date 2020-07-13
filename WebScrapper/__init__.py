a3b1a2db1b93e076e9a8de406a44a3bfc0a4a87e
Daenamu <you@dheodnr318@naver.com>
2020-07-13 16:15:47 +0900

    form and args

--- WebScrapper/__init__.py
+++ WebScrapper/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,3 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template("potato.html")
 
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html", word=word)
